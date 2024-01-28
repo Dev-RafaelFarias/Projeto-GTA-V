f04e8b5f25f5fd32d3aec917f8e6bb0f485e77c3
Dev-RafaelFarias <rafaeldevfarias@gmail.com>
2024-01-27 23:10:41 -0300

    Projeto GTA V Finalizado

---
+++ script.js
@@ -0,0 +1,18 @@
+const botao = document.querySelector(".btn-plataforma");
+
+const elementoPlataformas = document.querySelector(".btn-plataforma .plataformas");
+
+botao.addEventListener("click", () => {
+
+    const botaoAberto = elementoPlataformas.classList.contains("ativo");
+    
+    elementoPlataformas.classList.toggle("ativo")
+    
+    /*if(botaoAberto){
+        elementoPlataformas.classList.remove("ativo");
+    }
+    else{
+        elementoPlataformas.classList.add("ativo");
+    }*/
+
+});
